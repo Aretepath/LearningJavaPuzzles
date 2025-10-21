5c59826e42ad38dede3c34bad7e4ef606017ae7f
Richard, Clark <richard.clark@sky.uk>
2025-10-21 21:43:23 +0100

    INitial Commit

---
+++ src/paractice/strings/StringPractice.java
@@ -0,0 +1,43 @@
+package paractice.strings;
+
+
+public class StringPractice {
+public static void main(String[] args) {
+	//System.out.println("Hello World");
+	
+	int mikesAge = 10;
+	int bobsAge = 12;
+	
+	//conditional block
+	if(mikesAge != bobsAge) {
+		//do this
+		System.out.println("ages are different");
+	} else {
+		// else do this
+	    System.out.println("ages are the same");
+	    
+	}
+	
+	String a = "hello";
+	String b = "there";
+	
+	if(!a.equals("hello")) {
+		//do this
+	}
+	
+	if(b.equalsIgnoreCase("There")) {
+		System.out.println("Printed There");
+	}
+	
+	System.out.println(a.charAt(3));
+	
+	String str = "Hello there yogi there";
+	
+	int c = str.indexOf("there", 7);
+	int d = str.lastIndexOf("yogi");
+	
+	System.out.println(c); //left to right
+	System.out.println(d); //right to left
+
+}
+}
